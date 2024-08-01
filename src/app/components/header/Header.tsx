a114a77d93579f0645712c8f3fe3e24b46b62c12
Codey V. <codeyvanourkerk@gmail.com>
2024-08-01 17:27:02 +0000

    Only use hover with mouse; Change div to nav for header

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,17 +1,15 @@
 import HeaderAnchor from "./HeaderAnchor";
-import ThemeController from "../ThemeController";
 import "@/app/styles/header.css";
 
 export default async function Header() {
     return (
-        <div className="header flex w-auto mx-0 sticky top-0 drop-shadow">
-            <div className="navigation flex-1 items-center justify-end join mr-4 gap-4 sm:gap-3 md:gap-4 lg:gap-6 xl:gap-8">
-                <HeaderAnchor anchor_id="home" className="join-item">Home</HeaderAnchor> 
+        <div className="header flex w-auto mx-0 sticky top-0 drop-shadow h-14">
+            <nav className="navigation flex-1 items-center justify-end join mr-4 gap-4 sm:gap-3 md:gap-4 lg:gap-6 xl:gap-8">
+                <HeaderAnchor anchor_id="home" className="join-item">Home</HeaderAnchor>
                 <HeaderAnchor anchor_id="about" className="join-item">About Me</HeaderAnchor>
                 <HeaderAnchor anchor_id="projects" className="join-item">Projects</HeaderAnchor>
                 <HeaderAnchor anchor_id="contact" className="join-item">Contact</HeaderAnchor>
-            </div>
-            <ThemeController className="flex-initial w-auto ml-auto" />
+            </nav>
         </div>
     );
 }
