3620101ce449f5dcd1ca28d3dcbacdac0af28df2
Codey V. <codeyvanourkerk@gmail.com>
2024-08-12 06:11:15 +0000

    Attribute fix

--- src/app/components/contact/ContactForm.tsx
+++ src/app/components/contact/ContactForm.tsx
@@ -57,7 +57,7 @@ export default function ContactForm() {
                         : <div className='card-body text-center'>
                             Thank you!
                             <div className='btn btn-primary' onClick={() => document.getElementById(HOME_SECTION)?.scrollIntoView({ behavior: "smooth", inline: "start" })}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-up"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M12 5l0 14" /><path d="M18 11l-6 -6" /><path d="M6 11l6 -6" /></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-up"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M12 5l0 14" /><path d="M18 11l-6 -6" /><path d="M6 11l6 -6" /></svg>
                                 Back To Top
                             </div>
                         </div>}
