f3e41447ce64cd96c7ceec98d6061ec8208bc360
Codey V. <codeyvanourkerk@gmail.com>
2024-09-04 21:12:21 +0000

    Updated descriptions

--- src/app/components/AboutMe.tsx
+++ src/app/components/AboutMe.tsx
@@ -3,21 +3,22 @@ export default async function AboutMe() {
     <article className="px-auto prose mx-auto p-5">
       <h1>About Me</h1>
       <p>
-        I first started programming back in 2015 when my high school offered a
-        computer science class for the first time. Intrigued, I took the AP
-        version of the class, as I am always up for a challenge. This was also
-        an introduction to computers for me. The class used Java and I was not
-        so great at it, but I found it very interesting and it gave me a path
-        forward for college.
+        I began programming in 2015 when my high school introduced a computer
+        science class for the first time. Eager for a challenge, I enrolled in
+        the Advanced Placement course, even though it was my first exposure to
+        computers. The class focused on Java, which I initially struggled with
+        but found fascinating. This experience shaped my future path in college.
+        Four years later, I earned a B.S. in Game Design Engineering from the
+        University of Silicon Valley in San Jose, California.
       </p>
       <p>
-        Fast forward and I am now a Software Engineer with almost 5 years of
-        experience creating websites, add-ins, and tools. I have professionaly
-        created a game, 3 full stack sites, and numerous tools and add-ins. In
-        my previous position, I took charge of the technical aspects including
-        architecting, building, and deplyoing software and tools. The team was
-        very small and things were constantly changing, but I adapted well and
-        learned a lot in the process.
+        Today, I am a Software Engineer with nearly 5 years of experience in
+        creating websites, add-ins, and tools. My professional work includes
+        developing a game, building 3 full-stack websites, and crafting numerous
+        tools and add-ins. In my previous role, I was responsible for overseeing
+        the technical aspects of projects, including software architecture,
+        development, and deployment. Working in a small, dynamic team, I adapted
+        to constant changes and gained valuable experience along the way.
       </p>
     </article>
   );
--- src/app/components/projects/ProjectTimeline.tsx
+++ src/app/components/projects/ProjectTimeline.tsx
@@ -10,12 +10,26 @@ export default function ProjectTimeline() {
           startOrEnd="timeline-start"
           StartDate={new Date(2023, 7)}
           EndDate={new Date(2024, 6)}
-          ProjectDescription={`
-            Using Next.js and GCP, I developed a Quality Control (QC) Web App to automate manual spreadsheet QC. 
-            The front end features an admin dashboard to create webhooks (for automatic QC), test queries 
-            (including a number of preset queries to test with) and generate Google Looker Studio dashboards per project. 
-            I used Firebase Firestore to store the data, and Firebase Cloud Functions to automate data filtering.`}
-          Skills={["React.js", "Next.js", "Firebase", "GCP"]}
+          ProjectDescription={
+            <p className="mb-2.5 mt-0">
+              Using Next.js and GCP, I developed a Quality Control (QC) Web App
+              to automate a manual QC process. The front end features an admin
+              dashboard to create webhooks (for automatic QC), test queries
+              (including a number of preset queries to test with) and generates
+              Google Looker Studio dashboards per project. I used Firebase
+              Firestore to store the data, and Firebase Cloud Functions to
+              automate data filtering.
+            </p>
+          }
+          Skills={[
+            "React.js",
+            "Next.js",
+            "Firebase",
+            "GCP",
+            "Typescript",
+            "GraphQL",
+            "OpenID",
+          ]}
         >
           <div className="prose relative left-0 top-0 mb-5 ml-5 text-left">
             <h1>Projects</h1>
@@ -35,11 +49,16 @@ export default function ProjectTimeline() {
           startOrEnd="timeline-end"
           StartDate={new Date(2023, 5)}
           EndDate={new Date(2024, 6)}
-          ProjectDescription={`
-            Developed a tool to extract data from PDF store receipts and populate a Google Sheet. 
-            The tool features advanced column sorting, auto populating purchase dates, and a custom 
-            UI to apply formulas to multiple ranges. It also features syncing Gmail and Google Calendar 
-            with automated events dependent on bill due dates, preventing missed payments.`}
+          ProjectDescription={
+            <p className="mb-2.5 mt-0">
+              Developed a tool to extract data from PDF store receipts and
+              populate a Google Sheet. The tool features advanced column
+              sorting, auto populating purchase dates, and a custom UI to apply
+              formulas to multiple ranges. It also features syncing Gmail and
+              Google Calendar with automated events dependent on bill due dates,
+              preventing missed payments.
+            </p>
+          }
           Skills={["Apps Script", "G-Suite", "Typescript"]}
         />
         <hr />
@@ -56,10 +75,15 @@ export default function ProjectTimeline() {
           startOrEnd="timeline-start"
           StartDate={new Date(2023, 2)}
           EndDate={new Date(2023, 3)}
-          ProjectDescription={`
-            Developed a tool to generate a weekly meal plan using the Google Tasks API and Google Sheets. 
-            The tool creates a Google Task for every selected meal and a subtask for every meal ingredient,
-            allowing the user to check off ingredients in the grocery store as the go.`}
+          ProjectDescription={
+            <p className="mb-2.5 mt-0">
+              Developed a tool to generate a weekly meal plan using the Google
+              Tasks API and Google Sheets. This tool creates a Google Task for
+              every meal per day and a subtask for every meal ingredient. These
+              features allow users to check off ingredients when shopping in the
+              grocery store from the Google Task mobile app.
+            </p>
+          }
           Skills={["Apps Script", "G-Suite", "Typescript"]}
         />
         <hr />
@@ -72,14 +96,35 @@ export default function ProjectTimeline() {
           startOrEnd="timeline-end"
           StartDate={new Date(2020, 7)}
           EndDate={new Date(2021, 2)}
-          ProjectDescription={`
-            Using Unity, I developed a game to view 2D Revit data in a 3D environment.             
-            I utilized Unity Reflect to import Autodesk Revit Geometry. I then created 
-            an importer/exporter to bring in custom data the user can interact with.
-            The game fetaured 3 player controllers to enhance navigation of imported models, 
-            including mobile, point-to-point, and first-person perspectives. I embedded Unity 
-            WebGL into a .NET site, securing user access with Autodesk's OAuth 2.0`}
-          Skills={["OAuth", "C#", ".NET", "Unity"]}
+          ProjectDescription={
+            <p className="mb-2.5 mt-0">
+              Using Unity, I developed a game to view 2D Revit data in a 3D
+              environment. I utilized{" "}
+              <a
+                href="https://unity.com/products/unity-reflect-develop"
+                target="_blank"
+                className="link"
+              >
+                Unity Reflect
+              </a>{" "}
+              to import Autodesk Revit Geometry. Following, I created an
+              importer/exporter for a user to upload custom data. The game
+              featured 3 player controllers to enhance navigation of imported
+              models, including mobile, point-to-point, and first-person
+              perspectives. I embedded Unity WebGL into a .NET site, securing
+              user access with Autodesk&apos;s OAuth 2.0.
+            </p>
+          }
+          Skills={[
+            "OAuth",
+            "C#",
+            ".NET",
+            "Unity",
+            "HTML",
+            "CSS",
+            "Javascript",
+            "Tools Programming",
+          ]}
         />
         <hr />
       </li>
@@ -91,13 +136,16 @@ export default function ProjectTimeline() {
           startOrEnd="timeline-start"
           StartDate={new Date(2020, 0)}
           EndDate={new Date(2022, 2)}
-          ProjectDescription={`
-            Developed and managed a Autodesk Construction Cloud dashboard
-            to view big data at a glance. The dashboard features OAuth for
-            permissions, a data text viewer that reads data from an Autodesk 3D model,
-            Google Charts to visualize user and project data, and an admin dashboard
-            to manage 1000+ users.`}
-          Skills={["OAuth", "HTML5", "CSS", "Javascript"]}
+          ProjectDescription={
+            <p className="mb-2.5 mt-0">
+              Developed and maintained an Autodesk Construction Cloud dashboard
+              to view account-wide model data at a glance. The dashboard
+              features OAuth for permissions, a data text viewer to read data
+              from an Autodesk 3D model, Google Charts to visualize user and
+              project data, and an admin dashboard to manage 1000+ users.
+            </p>
+          }
+          Skills={["OAuth", "HTML", "CSS", "Javascript"]}
         />
         <hr />
       </li>
@@ -109,11 +157,15 @@ export default function ProjectTimeline() {
           startOrEnd="timeline-end"
           StartDate={new Date(2019, 0)}
           EndDate={new Date(2019, 4)}
-          ProjectDescription={`
-            Collaborated with an artist and level designer to develop a puzzle platformer in Unity. 
-            I implemented core features such as the player controller, a time-warp mechanic, and all 
-            puzzles. I supported others with technical issues regarding Unity and Perforce (version control) 
-            to ensure team success.`}
+          ProjectDescription={
+            <p className="mb-2.5 mt-0">
+              Collaborated with an artist and level designer to develop a puzzle
+              platformer game in Unity. I implemented core features such as the
+              player controller, a time-warp mechanic, and all puzzles. I
+              supported the project team with technical issues regarding Unity
+              and Perforce.
+            </p>
+          }
           Skills={["Unity", "C#"]}
         />
         <hr />
@@ -128,10 +180,7 @@ export default function ProjectTimeline() {
           EndDate={new Date(2019, 4)}
           ProjectDescription={
             <p className="mb-2.5 mt-0">
-              Developed and designed an infinite runner game using C# and Unity.
-              Worked with an artist to effeciently integrate reusable assets
-              infinitely. The game features obstacles, powerups,and increasing
-              speed, and took inspiration from{" "}
+              Inspired by{" "}
               <a
                 className="link"
                 href="https://en.wikipedia.org/wiki/Temple_Run"
@@ -139,7 +188,10 @@ export default function ProjectTimeline() {
               >
                 Temple Run
               </a>
-              .
+              , I developed and designed an infinite runner game using C# and
+              Unity. Worked with an artist to streamline integration of reusable
+              assets infinitely. The game features obstacles, powerups, and
+              increasing speed.
             </p>
           }
           Skills={["Unity", "C#"]}
@@ -164,11 +216,16 @@ export default function ProjectTimeline() {
           startOrEnd="timeline-end"
           StartDate={new Date(2018, 8)}
           EndDate={new Date(2018, 11)}
-          ProjectDescription={`
-            Collaborted in a class-wide project with animators, tech artists, and lighting artists
-            to create a short animated film. I developed Autodesk Maya tools to streamline repetitive tasks
-            and utilized the Autodesk Maya Python API to automate and export render layers for the render farm.
-            I imporved rendering performance by Developing a shader clean-up tool to remove duplicate shaders.`}
+          ProjectDescription={
+            <p className="mb-2.5 mt-0">
+              Collaborated in a class-wide project with animators, tech artists,
+              and lighting artists to create a short animated film. I developed
+              Autodesk Maya tools to streamline repetitive tasks and utilized
+              the Autodesk Maya Python API to automate and export render layers
+              for the render farm. I improved rendering performance by
+              developing a shader clean-up tool to remove duplicate shaders.
+            </p>
+          }
           Skills={["Maya", "Python", "Rendering"]}
         />
         <hr />
@@ -185,11 +242,15 @@ export default function ProjectTimeline() {
           startOrEnd="timeline-start"
           StartDate={new Date(2018, 8)}
           EndDate={new Date(2018, 11)}
-          ProjectDescription={`
-            Collaborted in a class-wide project with artists, audio engineers, and other programmers 
-            to create a AR Unity Game. As the lead engineer for the project, I
-            implementing core gameplay mechanics, managed other engineers, and 
-            delegated tasks to ensure success.`}
+          ProjectDescription={
+            <p className="mb-2.5 mt-0">
+              Lead engineer for a class-wide project with artists, audio
+              engineers, and other programmers to create an Augmented Reality
+              (AR) Unity Game. I implemented the core gameplay mechanics,
+              provided support to other engineers, and delegated tasks to ensure
+              the project&apos;s success.
+            </p>
+          }
           Skills={["Unity", "C#", "Augmented Reality"]}
         />
         <hr />
@@ -204,9 +265,8 @@ export default function ProjectTimeline() {
           EndDate={new Date(2018, 11)}
           ProjectDescription={
             <p className="mb-2.5 mt-0">
-              Using Kotlin and Android Studio, I developed an app for the first
-              time for a Mobile programming class. The app can calculate the
-              user&apos;s{" "}
+              Using Kotlin and Android Studio, I developed a mobile Android
+              application. The app can calculate a user&apos;s{" "}
               <a
                 className="link"
                 target="_blank"
@@ -214,12 +274,12 @@ export default function ProjectTimeline() {
               >
                 Wilks Score
               </a>
-              , can calculate an estimated 1 rep-max based on number of reps for
-              a given weight, and can notetake. The user&apos;s previous inputs
-              are automatically saved for convienence.
+              , an estimated 1 rep-max based on number of reps for a given
+              weight, and includes note taking features. The user&apos;s
+              previous inputs are also automatically saved for future reuse.
             </p>
           }
-          Skills={["Android App", "Kotin", "Strength Training"]}
+          Skills={["Mobile Development", "Kotlin"]}
         />
         <hr />
       </li>
@@ -235,12 +295,16 @@ export default function ProjectTimeline() {
           startOrEnd="timeline-start"
           StartDate={new Date(2018, 0)}
           EndDate={new Date(2018, 3)}
-          ProjectDescription={`
-            In a Game Design class, I engineered and designed a horror game 
-            using Unity and C#. I worked on a team of 4 as the lead programmer, 
-            implementing core game mechanics and systems such as
-            the character controller, progression system, camera work (i.e. cutscenes).`}
-          Skills={["Unity", "C#", "Lead", "Level Design"]}
+          ProjectDescription={
+            <p className="mb-2.5 mt-0">
+              In a Game Design class, I programmed and designed a horror game
+              using Unity and C#. I worked on a team of 4 as the lead
+              programmer, implementing core game mechanics and systems such as
+              the character controller, progression system, camera work (i.e.
+              cutscenes).
+            </p>
+          }
+          Skills={["Unity", "C#", "Level Design"]}
         />
         <hr />
       </li>
@@ -256,11 +320,15 @@ export default function ProjectTimeline() {
           startOrEnd="timeline-end"
           StartDate={new Date(2018, 0)}
           EndDate={new Date(2018, 3)}
-          ProjectDescription={`
-            Using Python and Autodesk Maya, I designed and created an Autorigging tool. 
-            The tool was developed for a Tools programming class and used in a separate 
-            Technical Rigging class. The tool features a UI with configurable options
-            that allow the user to change the humanoid rig to their needs.`}
+          ProjectDescription={
+            <p className="mb-2.5 mt-0">
+              Using Python and Autodesk Maya, I designed and created an
+              Autorigging tool. The tool features a UI with configurable options
+              that allow a user to adapt the humanoid rig to their needs. The
+              tool was developed for a Tools Programming class and later used in
+              a Technical Rigging class.
+            </p>
+          }
           Skills={["Maya", "Python", "Technical Rigging", "Tools Programming"]}
         />
         <hr />
