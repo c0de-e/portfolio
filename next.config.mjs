543e2be8fc089be991186f0619e6204d86f9a7b2
Codey V. <codeyvanourkerk@gmail.com>
2026-01-15 20:27:04 +0000

    Made headers async

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    headers: [
-        {
-            source: '/',
-            headers: [
-                {
-                    key: 'Cache-Control',
-                    value: 'public, max-age=0, must-revalidate',
-                },
-            ],
-        },
-        {
-            source: '/_next/static/:path*',
-            headers: [
-                {
-                    key: 'Cache-Control',
-                    value: 'public, max-age=31536000, immutable',
-                },
-            ],
-        },
-    ]
+    async headers() {
+        return [
+            {
+                source: '/',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=0, must-revalidate',
+                    },
+                ],
+            },
+            {
+                source: '/_next/static/:path*',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=31536000, immutable',
+                    },
+                ],
+            },
+        ];
+    }
 };
 
 export default nextConfig;
