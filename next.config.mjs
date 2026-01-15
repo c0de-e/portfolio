9c744b7f0404fb632a685b6e67bf34119e921140
Codey V. <codeyvanourkerk@gmail.com>
2026-01-15 19:49:33 +0000

    Updated caching

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone'
+    output: 'standalone',
+    headers: [
+        {
+            key: 'Cache-Control',
+            value: 'public, s-maxage=60, stale-while-revalidate=86400',
+        }
+    ]
 };
 
 export default nextConfig;
